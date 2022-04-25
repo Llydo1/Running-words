aed29ba3de3e0a6ea46cb4d002ea69b8cf512c29
Llydo1 <94525521+Llydo1@users.noreply.github.com>
2022-04-25 21:16:40 +0700

    Still testing

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let element = document.getElementById('Hehe');
+let element1 = document.getElementById('Hehe'), element2 = document.getElementById('button');
+let element3 = document.getElementById('number');
+let number = 0;
 let count = 0;
 let click = 0 ;
 let control = 0;
 
 function run(){
     if(control ===0){
-        console.log(count);
         count++;
         document.getElementById('Hehe').style.left = count +'px';
         if(count === 1000){
             control =1;
         }
     }else{
-        console.log(count);
         count--;
-        element.style.left = count +'px';
+        element1.style.left = count +'px';
         if(count===0) control =0;
     }
 }
@@ -29,7 +29,7 @@ function print(){
 //     }
 // },5);   
 start();
-element.onclick = () => {
+element1.onclick = () => {
     if (click === 0) {
         click = 1;
     } else {
@@ -45,4 +45,19 @@ function start(){
             clearInterval(refresh);
         }
     },5);   
+}
+
+element2.onclick = () =>{
+    number++;
+    element3.innerText = 
+    `${number}x2 = ${number*2} 
+    ${number}x3 = ${number*3}
+    ${number}x4 = ${number*4}
+    ${number}x5 = ${number*5}
+    ${number}x6 = ${number*6}
+    ${number}x7 = ${number*7}
+    ${number}x8 = ${number*8}
+    ${number}x9 = ${number*9}
+    ${number}x10 = ${number*10}
+                                    `
 }
